61851e0188a0f83dda25e85e8451831c337d3ba0
ChengKang520 <42560067+ChengKang520@users.noreply.github.com>
2021-11-29 13:15:09 +0800

    Add files via upload

---
+++ ClassifyingHD/main_train_HD.m
@@ -0,0 +1,194 @@
+
+
+%%*************************************************************************
+% To analyze the EEG data by deep learning
+% C Kang
+% To clsssify the depression and control based on 0-back
+% kangkangsome@gmail.com
+%%*************************************************************************
+
+clear all;
+close all;
+workspace_addr = '..\'
+cd(workspace_addr);
+
+tasks = 3; % 0-back, 1-back, 2-back
+Trail_numH = [60 58 59 58 58 22 57 60 56 56 58 58 58 49 59;
+    57 55 60 56 56 14 49 59 58 57 56 54 58 45 59;
+    58 53 54 58 55 17 56 54 59 55 54 52 58 60 56];
+load('.\ScoringDepression_nback\Trail_back0_numD.mat');
+load('.\ScoringDepression_nback\Trail_back1_numD.mat');
+load('.\ScoringDepression_nback\Trail_back2_numD.mat');
+Trail_numD = [Trail_back0_numD; Trail_back1_numD; Trail_back2_numD];
+
+% K fold cross validation
+Kfold = 5;
+indicesH = crossvalind('Kfold', size(Trail_numH, 2), Kfold);
+indicesD = crossvalind('Kfold', size(Trail_numD, 2), Kfold);
+
+Result_all = cell(2,tasks);
+Result_all_Nback = cell(1,Kfold);
+for k = 1:Kfold
+    k
+    for task = 1:tasks
+        task
+        %% load data
+        if task == 1
+            EEGDataH = load('./NbackData/EEGData_Hback0.mat');
+            size_EEGDataH = size(EEGDataH.EEGdata_back0);
+            
+            EEGDataD = load('.\ScoringDepression_nback\EEGdata_back0.mat');
+            size_EEGDataD = size(EEGDataD.EEGdata_back0);
+            % distribute EEG data
+            EEGDataD = reshape(EEGDataD.EEGdata_back0(:,:,1:64*18), [size_EEGDataD(1), size_EEGDataD(2), 64, 18]);
+            %         EEGDataD.EEGdata_back0 = permute(EEGDataD.EEGdata_back0, [2 3 4 1]);
+            EEGDataH = reshape(EEGDataH.EEGdata_back0(:,:,1:64*18), [size_EEGDataH(1), size_EEGDataH(2), 64, 18]);
+            %         EEGDataH.EEGdata_back0 = permute(EEGDataH.EEGdata_back0, [2 3 4 1]);
+            
+        else if task == 2
+                EEGDataH = load('./NbackData/EEGData_Hback1.mat');
+                size_EEGDataH = size(EEGDataH.EEGdata_back1);
+                
+                EEGDataD = load('.\ScoringDepression_nback\EEGdata_back1.mat');
+                size_EEGDataD = size(EEGDataD.EEGdata_back1);
+                % distribute EEG data
+                EEGDataD = reshape(EEGDataD.EEGdata_back1(:,:,1:64*18), [size_EEGDataD(1), size_EEGDataD(2), 64, 18]);
+                %         EEGDataD.EEGdata_back1 = permute(EEGDataD.EEGdata_back1, [2 3 4 1]);
+                EEGDataH = reshape(EEGDataH.EEGdata_back1(:,:,1:64*18), [size_EEGDataH(1), size_EEGDataH(2), 64, 18]);
+                %         EEGDataH.EEGdata_back1 = permute(EEGDataH.EEGdata_back1, [2 3 4 1]);
+                
+            else if task == 3
+                    EEGDataH = load('./NbackData/EEGData_Hback2.mat');
+                    size_EEGDataH = size(EEGDataH.EEGdata_back2);
+                    
+                    EEGDataD = load('.\ScoringDepression_nback\EEGdata_back2.mat');
+                    size_EEGDataD = size(EEGDataD.EEGdata_back2);
+                    % distribute EEG data
+                    EEGDataD = reshape(EEGDataD.EEGdata_back2(:,:,1:64*18), [size_EEGDataD(1), size_EEGDataD(2), 64, 18]);
+                    %         EEGDataD.EEGdata_back2 = permute(EEGDataD.EEGdata_back2, [2 3 4 1]);
+                    EEGDataH= reshape(EEGDataH.EEGdata_back2(:,:,1:64*18), [size_EEGDataH(1), size_EEGDataH(2), 64, 18]);
+                    %         EEGDataH.EEGdata_back2 = permute(EEGDataH.EEGdata_back2, [2 3 4 1]);
+                    
+                end
+            end
+        end
+        
+        %  data redistribute;
+        testH = (indicesH == k);  %test index
+        trainH = ~testH;  %train index
+        testD = (indicesD == k); %test index
+        trainD = ~testD;  %train index
+        
+        % distribute and arrange the data of depression and healthy
+        trainDataH = [];
+        testDataH = [];
+        for iH = 1: length(testH)
+            Data_temp = [];
+            to_H = sum(Trail_numH(task,1:iH));
+            from_H = to_H - Trail_numH(task,iH) + 1;
+            
+            Data_temp = EEGDataH(from_H:to_H,:,:,:);
+            if testH(iH) == 0
+                trainDataH = [trainDataH; Data_temp];
+            elseif testH(iH) == 1
+                testDataH = [testDataH; Data_temp];
+            end
+        end
+        
+        trainDataD = [];
+        testDataD = [];
+        for iD = 1: length(testD)
+            Data_temp = [];
+            to_D = sum(Trail_numD(task, 1:iD));
+            from_D = to_D - Trail_numD(task,iD) + 1;
+            
+            Data_temp = EEGDataD(from_D:to_D,:,:,:);
+            if testD(iD) == 0
+                trainDataD = [trainDataD; Data_temp];
+            elseif testD(iD) == 1
+                testDataD = [testDataD; Data_temp];
+            end
+        end
+        clear EEGDataD
+        clear EEGDataH
+        
+        train = []; validation = []; test = []; 
+        train.Features = [trainDataH; trainDataD];
+        train.Labels = categorical([ones(1,size(trainDataD,1))*2 ones(1,size(trainDataH,1))]);
+        range_data = randperm(length(train.Labels));
+        train.Features = train.Features(range_data,:,:,:);
+        train.Features = permute(train.Features, [2 3 4 1]);
+        train.Labels = train.Labels(range_data);
+        
+        validationLength = floor((size(trainDataH,1)+size(trainDataD,1))*0.1);
+        validation.Features = train.Features(:,:,:,1:validationLength);
+        validation.Labels = train.Labels(1:validationLength);
+        
+        test.Features = [testDataH; testDataD];
+        test.Features = permute(test.Features, [2 3 4 1]);
+        test.Labels = categorical([ones(1,size(testDataD,1))*2 ones(1,size(testDataH,1))]);
+                
+        %% Construct Network
+        numClasses = 2; % the number of classes
+        netWidth = 64;
+        numUnits = 6;
+        lgraph = residualCIFARlgraph(netWidth,numUnits,"standard");
+        %     analyzeNetwork(lgraph); % show the structure of designed neural network
+        
+        miniBatchSize = 64;
+        learnRate = 0.01;
+        options = trainingOptions('adam', ...
+            'InitialLearnRate',learnRate, ...
+            'MaxEpochs',15, ...
+            'MiniBatchSize',miniBatchSize, ...
+            'Shuffle','every-epoch', ...
+            'Verbose',false, ...
+            'ValidationData',{validation.Features, validation.Labels'}, ...
+            'LearnRateSchedule','piecewise', ...
+            'LearnRateDropFactor',0.1, ...
+            'LearnRateDropPeriod',10);
+        
+        netTransfer = trainNetwork(train.Features, train.Labels', lgraph, options);
+        
+        %% Predict and Plot figures
+        predictedLabels = classify(netTransfer, test.Features);
+        accuracy_matrix(k) = sum(predictedLabels==test.Labels')/numel(predictedLabels)
+        
+        %% Plot the single person's performance
+        Trail_testH = [];
+        Trail_testH = Trail_numH(task,:).*testH';
+        Trail_testH(find(Trail_testH==0)) = [];
+        figure;
+        for i = 1:length(Trail_testH)
+            to_i = sum(Trail_testH(1:i));
+            from_i = to_i - Trail_testH(i) + 1;
+            accuracy_H(i) = sum(predictedLabels(from_i:to_i)==test.Labels(from_i:to_i)')/numel(predictedLabels(from_i:to_i));
+            subplot(length(Trail_testH),1,i);
+            plot(predictedLabels(from_i:to_i),'r');hold on;
+            plot(test.Labels(from_i:to_i),'k');hold off;
+            grid on;
+        end
+        Trail_testD = [];
+        Trail_testD = Trail_numD(task,:).*testD';
+        Trail_testD(find(Trail_testD==0)) = [];
+        figure;
+        for j = 1:length(Trail_testD)
+            to_j = sum(Trail_testD(1:j)) + to_i;
+            from_j = to_j - Trail_testD(j) + 1;
+            accuracy_D(j) = sum(predictedLabels(from_j:to_j)==test.Labels(from_j:to_j)')/numel(predictedLabels(from_j:to_j));
+            subplot(length(Trail_testD),1,j);
+            plot(predictedLabels(from_j:to_j),'r');hold on;
+            plot(test.Labels(from_j:to_j),'k');hold off;
+            grid on;
+        end
+        Result_all(1,task) = {accuracy_H};
+        Result_all(2,task) = {accuracy_D};
+    end
+    
+    Result_all_Nback(k) = {Result_all};
+    
+end
+
+save Result_all_Nback Result_all_Nback
+
+
